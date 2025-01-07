ff3609c3b5945a9228e88a43a91a2e3458e324a7
miguel-pub <miguelebersbach+1@outlook.com>
2025-01-07 14:41:01 +0100

    Learning

---
+++ JavaScriptCourse/complete-javascript-course/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,12 @@
+const chosenCountryName = "Germany";
+const chosenCountryContinent = "Europe";
+let chosenCountryPopulation = 83000000;
+
+const isIsland = false;
+let language;
+
+language = "German"
+
+console.log(typeof chosenCountryContinent, typeof chosenCountryName, typeof chosenCountryPopulation, typeof isIsland);
+
+console.log(chosenCountryContinent, chosenCountryName, chosenCountryPopulation, language)
