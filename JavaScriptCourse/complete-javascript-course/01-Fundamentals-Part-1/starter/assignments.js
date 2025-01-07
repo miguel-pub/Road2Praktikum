3c5c6a2ca8e0e92e1331c9db2379fec415dc7930
miguel-pub <miguelebersbach+1@outlook.com>
2025-01-07 15:11:36 +0100

    Update assignments.js

--- JavaScriptCourse/complete-javascript-course/01-Fundamentals-Part-1/starter/assignments.js
+++ JavaScriptCourse/complete-javascript-course/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,12 +1,27 @@
-const chosenCountryName = "Germany";
-const chosenCountryContinent = "Europe";
-let chosenCountryPopulation = 83000000;
+// const chosenCountryName = "Germany";
+// const chosenCountryContinent = "Europe";
+// let chosenCountryPopulation = 83000000;
 
-const isIsland = false;
-let language;
+// const isIsland = false;
+// let language;
 
-language = "German"
+// language = "German"
 
-console.log(typeof chosenCountryContinent, typeof chosenCountryName, typeof chosenCountryPopulation, typeof isIsland);
+// console.log(typeof chosenCountryContinent, typeof chosenCountryName, typeof chosenCountryPopulation, typeof isIsland);
 
-console.log(chosenCountryContinent, chosenCountryName, chosenCountryPopulation, language)
+// console.log(chosenCountryContinent, chosenCountryName, chosenCountryPopulation, language)
+
+// This is a comment
+
+/* this is a multiline comment
+
+middle
+
+
+end
+*/
+
+let age = 30;
+
+age = 50;
+
