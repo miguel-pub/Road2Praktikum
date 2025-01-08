3c6a03b9e1563df902cf2054d47b34fd824cd9fe
miguel-pub <miguelebersbach+1@outlook.com>
2025-01-08 13:14:15 +0100

    Moar

---
+++ jsYoutubeCourse/index.js
@@ -0,0 +1,61 @@
+// document.getElementById("myH1").textContent = "Hello!";
+// document.getElementById("myP").textContent = "This is a paragraph!";
+
+// let x;
+// x = 123;
+
+// let y = 321;
+
+// console.log(x,y);
+
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+
+// `` let's you use {VARIABLE}, similar to Python f-strings
+// console.log(typeof gpa)
+// console.log(`You are ${age} years old.`)
+// console.log(`The price is $${price}`)
+// console.log(`Your gpa is: ${gpa}`)
+
+// let firstName = "Bro"
+// let favFood = "Pizza"
+// let email = "bro@gmail.com"
+
+// console.log(firstName, typeof firstName)
+// console.log(`Your name is ${firstName}`)
+// console.log(`Your fav food is ${favFood}`)
+// console.log(`Your email is ${email}`)
+
+// let online = true;
+// let forSale = true;
+
+// console.log(`Bro is online: ${online}`);
+// console.log(`Is this car for sale: ${forSale}`);
+
+// let fullName = "Miguel E";
+// let age = 26;
+// let student = true;
+
+// document.getElementById("p1").textContent = `Your name is ${fullName}`;
+// document.getElementById("p2").textContent = `You are ${age} old.`;
+// document.getElementById("p3").textContent = `Enrolled: ${student}`;
+
+// let students = 30;
+
+// students = students + 1
+
+// //students++; increments by one
+// //students--; decrements by one
+
+// console.log(students)
+
+// let username;
+// username = window.prompt("What is your username?")
+// console.log(username)
+// let username;
+// document.getElementById("mySubmit").onclick = function(){
+//     username = document.getElementById("myText").value;
+//     document.getElementById("myH1").textContent = `Hello ${username}`;
+// }
+
