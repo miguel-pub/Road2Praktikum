1e0df9a0b41bd69c4a32535012ed862f0ddbca00
miguel-pub <miguelebersbach+1@outlook.com>
2025-01-15 11:57:48 +0100

    ternary

---
+++ jsYoutubeCourse/ternaryoperator/index.js
@@ -0,0 +1,14 @@
+// if else shortcuts??
+
+// let age = 21;
+
+// message = age >= 18 ? "You're an adult" : "You are a minor"
+
+// console.log(message)
+
+// let time = 9;
+
+// let greeting = time < 12 ? "Good morning" : "Good afternoon"
+
+// console.log(greeting)
+
